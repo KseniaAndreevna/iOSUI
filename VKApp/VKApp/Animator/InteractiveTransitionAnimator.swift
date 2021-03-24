bbf536ca58207ed0a0eba40f132feb07339c20e0
Ksenia <elleza@mail.ru>
2021-03-24 17:00:44 +0300

    Lesson11

---
+++ VKApp/VKApp/Animator/InteractiveTransitionAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  InteractiveTransitionAnimator.swift
+//  Weather
+//
+//  Created by andrey.antropov on 10.03.2021.
+//
+
+import UIKit
+
+class InteractiveTransitionAnimator: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
